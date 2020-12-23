2f13c2bdc85993d79943df48157dc10089a30438
Katsuyuki Itoh <k.itoh@kufusha.com>
2020-12-23 13:14:14 +0900

    Fix oss license dialog

--- README.md
+++ README.md
@@ -10,8 +10,3 @@
 6. $ ./build-AsionAlphaUI-unknown-Debug/AsionAlphaUI
 
 ToDo: We must make shellscript to setup ros environment variables just like wcfk_ui.sh!!
-
-## How to create licenses/third-party-licenses.txt
-third-party-licenses.txtは仮で作成したものです。
-1. $ cd AsionAlphaUI/licenses
-2. $ cat /usr/share/common-licenses/* > third-party-licenses.txt
--- licenses/osscopyright.sh
+++ licenses/osscopyright.sh
@@ -1,15 +1,42 @@
 #!/bin/sh
 
 FILE="/home/kufushatec/AsionAlphaUI/licenses/osslic.html"
+OSSLIC="/home/kufushatec/AsionAlphaUI/licenses/osslicenses.html"
 
 echo "<html>" > ${FILE}
 echo "<head><title>OSS Licenses</title></head>" >> ${FILE}
 echo "<body>" >> ${FILE}
 
+
+
+echo "<h1>Third-party materials and licenses</h1>" >> ${FILE}
+
 find /usr/share/doc -name copyright | \
     sed "s/\/usr\/share\/doc\///" | sed "s/\/copyright//" | \
-    awk '{print "<a href=\"/usr/share/doc/" $1 "/copyright\">" $1 "<br>"}' | \
+    awk '{print "<a href=\"/usr/share/doc/" $1 "/copyright\">" $1 "</a><br>"}' | \
     sort >> ${FILE}
+
+find /home/kufushatec/AsionAlphaUI/licenses/OSS -name COPYING.LGPL | \
+    sed "s/\/home\/kufushatec\/AsionAlphaUI\/licenses\/OSS\///" | sed "s/\/COPYING.LGPL//" | \
+    awk '{print "<a href=\"/home/kufushatec/AsionAlphaUI/licenses/OSS/" $1 "/COPYING.LGPL\">" $1 "</a><br>"}' | \
+    sort >> ${FILE}
+
+find /home/kufushatec/AsionAlphaUI/licenses/OSS -name LICENSE | \
+    sed "s/\/home\/kufushatec\/AsionAlphaUI\/licenses\/OSS\///" | sed "s/\/LICENSE//" | \
+    awk '{print "<a href=\"/home/kufushatec/AsionAlphaUI/licenses/OSS/" $1 "/LICENSE\">" $1 "</a><br>"}' | \
+    sort >> ${FILE}
+
+find /home/kufushatec/AsionAlphaUI/licenses/OSS -name PackageSummary | \
+    sed "s/\/home\/kufushatec\/AsionAlphaUI\/licenses\/OSS\///" | sed "s/\/PackageSummary//" | \
+    awk '{print "<a href=\"/home/kufushatec/AsionAlphaUI/licenses/OSS/" $1 "/PackageSummary\">" $1 "</a><br>"}' | \
+    sort >> ${FILE}
+
+find /home/kufushatec/AsionAlphaUI/licenses/OSS -name COPYRIGHT.txt | \
+    sed "s/\/home\/kufushatec\/AsionAlphaUI\/licenses\/OSS\///" | sed "s/\/COPYRIGHT.txt//" | \
+    awk '{print "<a href=\"/home/kufushatec/AsionAlphaUI/licenses/OSS/" $1 "/COPYRIGHT.txt\">" $1 "</a><br>"}' | \
+    sort >> ${FILE}
+
+cat ${OSSLIC} >> ${FILE}
 
 echo "</body>" >> ${FILE}
 echo "</html>" >> ${FILE}
